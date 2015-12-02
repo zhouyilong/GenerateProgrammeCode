30f89077f8e9e119542bd47ce96510ddd7aec2a4
zhouyilong <1023471981@qq.com>
2015-12-02 22:36:05 +0800

    生成代码

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,191 @@ namespace GenerateProgrammeCode
         {
             InitializeComponent();
         }
+
+        //string
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = tb.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                string[] properties = text.Split(',');
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var item in properties)
+                {
+                    if (item.Split(':').Length == 2)
+                    {
+                        sb.Append(GetStringPropertyCode(item.Split(':')[0], item.Split(':')[1]));
+                        sb.AppendLine();
+                    }
+                }
+                rtb.Document.Blocks.Clear();
+                rtb.AppendText(sb.ToString());
+
+            }
+        }
+
+
+
+        //datetime
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string text = tb.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                string[] properties = text.Split(',');
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var item in properties)
+                {
+                    if (item.Split(':').Length == 2)
+                    {
+                        sb.Append(GetDatetimePropertyCode(item.Split(':')[0], item.Split(':')[1]));
+                        sb.AppendLine();
+                    }
+                }
+                rtb.Document.Blocks.Clear();
+                rtb.AppendText(sb.ToString());
+
+            }
+        }
+
+        //number
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string text = tb.Text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                string[] properties = text.Split(',');
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var item in properties)
+                {
+                    if (item.Split(':').Length == 2)
+                    {
+                        sb.Append(GetNumberPropertyCode(item.Split(':')[0], item.Split(':')[1]));
+                        sb.AppendLine();
+                    }
+                }
+                rtb.Document.Blocks.Clear();
+                rtb.AppendText(sb.ToString());
+
+            }
+        }
+
+
+        StringBuilder GetStringPropertyCode(string name, string note)
+        {
+            string _name = "_" + (name.ToLower().Replace("_", ""));
+            StringBuilder template = new StringBuilder();
+            template.AppendFormat("private string {0};", _name);
+            template.AppendLine();
+            template.Append("/// <summary>");
+            template.AppendLine();
+            template.AppendFormat("///{0}", note);
+            template.AppendLine();
+            template.Append("/// </summary>");
+            template.AppendLine();
+            template.AppendFormat("[Column(Storage = \"{0}\", Name = \"{1}\", DbType = \"VARCHAR2\", AutoSync = AutoSync.Never)]"
+                , _name, name);
+            template.AppendLine();
+            template.Append("[DebuggerNonUserCode()]");
+            template.AppendLine();
+            template.AppendFormat("public string {0}", name);
+            template.AppendLine();
+            template.Append("{get{");
+            template.AppendLine();
+            template.AppendFormat("return this.{0};", _name);
+            template.AppendLine();
+            template.Append("}set{");
+            template.AppendLine();
+            template.AppendFormat(" if (({0} == value) == false)",
+                _name);
+            template.AppendLine();
+            template.Append("{");
+            template.AppendLine();
+            template.AppendFormat("this.{0} = value;this.OnPropertyChanged(\"{1}\");", _name, name);
+            template.AppendLine();
+            template.Append("}}}");
+
+            return template;
+        }
+
+        StringBuilder GetDatetimePropertyCode(string name, string note)
+        {
+            string _name = "_" + (name.ToLower().Replace("_", ""));
+            StringBuilder template = new StringBuilder();
+            template.AppendFormat("private System.Nullable<System.DateTime> {0};", _name);
+            template.AppendLine();
+            template.Append("/// <summary>");
+            template.AppendLine();
+            template.AppendFormat("///{0}", note);
+            template.AppendLine();
+            template.Append("/// </summary>");
+            template.AppendLine();
+            template.AppendFormat("[Column(Storage = \"{0}\", Name = \"{1}\", DbType = \"DATE\", AutoSync = AutoSync.Never)]"
+                , _name, name);
+            template.AppendLine();
+            template.Append("[DebuggerNonUserCode()]");
+            template.AppendLine();
+            template.AppendFormat("public System.Nullable<System.DateTime> {0}", name);
+            template.AppendLine();
+            template.Append("{get{");
+            template.AppendLine();
+            template.AppendFormat("return this.{0};", _name);
+            template.AppendLine();
+            template.Append("}set{");
+            template.AppendLine();
+            template.AppendFormat(" if (({0} == value) == false)",
+                _name);
+            template.AppendLine();
+            template.Append("{");
+            template.AppendLine();
+            template.AppendFormat("this.{0} = value;this.OnPropertyChanged(\"{1}\");", _name, name);
+            template.AppendLine();
+            template.Append("}}}");
+
+            return template;
+        }
+
+        StringBuilder GetNumberPropertyCode(string name, string note)
+        {
+            string _name = "_" + (name.ToLower().Replace("_", ""));
+            StringBuilder template = new StringBuilder();
+            template.AppendFormat("private System.Nullable<decimal> {0};", _name);
+            template.AppendLine();
+            template.Append("/// <summary>");
+            template.AppendLine();
+            template.AppendFormat("///{0}", note);
+            template.AppendLine();
+            template.Append("/// </summary>");
+            template.AppendLine();
+            template.AppendFormat("[Column(Storage = \"{0}\", Name = \"{1}\", DbType = \"NUMBER\", AutoSync = AutoSync.Never)]"
+                , _name, name);
+            template.AppendLine();
+            template.Append("[DebuggerNonUserCode()]");
+            template.AppendLine();
+            template.AppendFormat("public System.Nullable<decimal> {0}", name);
+            template.AppendLine();
+            template.Append("{get{");
+            template.AppendLine();
+            template.AppendFormat("return this.{0};", _name);
+            template.AppendLine();
+            template.Append("}set{");
+            template.AppendLine();
+            template.AppendFormat(" if (({0} == value) == false)",
+                _name);
+            template.AppendLine();
+            template.Append("{");
+            template.AppendLine();
+            template.AppendFormat("this.{0} = value;this.OnPropertyChanged(\"{1}\");", _name, name);
+            template.AppendLine();
+            template.Append("}}}");
+
+            return template;
+        }
     }
 }
