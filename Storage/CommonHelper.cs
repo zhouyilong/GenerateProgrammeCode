cd8f8feda2b1338b092ed553897979e10bdb8bdc
zhouyilong <1023471981@qq.com>
2015-12-14 23:07:19 +0800

    增加公共帮助类

---
+++ Storage/CommonHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GenerateProgrammeCode.Storage
+{
+    public class CommonHelper
+    {
+
+    }
+
+    /// <summary>
+    /// <para>日志帮助类</para>
+    /// <para>by zhouyilong 20151214</para>
+    /// </summary>
+    public class LogHelper
+    {
+        /// <summary>
+        /// <para>写入日志</para>
+        /// <para>by zhouyilong 20151214</para>
+        /// </summary>
+        /// <param name="str">需要写入的字符</param>
+        public static void WriteLog(string str)
+        {
+
+            string strLog = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "  " + str + "/r/n";
+
+            StreamWriter errorlog = new StreamWriter(System.IO.Path.Combine(Environment.CurrentDirectory, @"log.txt"), true);
+            errorlog.WriteLine(strLog);
+            errorlog.Flush();
+            errorlog.Close();
+        }
+    }
+}
