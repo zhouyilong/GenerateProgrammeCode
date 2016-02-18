8ac447ea7af0f41404802a8f4281834be1c3c994
zhouyilong <1023471981@qq.com>
2016-02-18 17:23:46 +0800

    增加域配置SQL生成

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -39,118 +39,6 @@ namespace GenerateProgrammeCode
         public MainWindow()
         {
             InitializeComponent();
-
-            //检查是否有更新
-            bool isNeedUpdate = false;
-            bool isMustUpdate=false;
-            url = ConfigurationManager.AppSettings["Url"].Trim();
-            version = ConfigurationManager.AppSettings["Version"].Trim();
-            if (string.IsNullOrEmpty(version))
-            {
-                isNeedUpdate = true;
-            }
-            else if (!string.IsNullOrEmpty(url))
-            {
-                WebClient client = new WebClient();
-                try
-                {
-                    Uri address = new Uri(url);
-
-                    string serverJson=client.DownloadString(address);
-                    serverJObject = JToken.Parse(serverJson) as JObject;
-
-
-                    //if(serverJObject!=null)
-                    //{
-
-                    //}
-
-                    string serverVersion = serverJObject["version"].ToString();
-                    if(!string.IsNullOrEmpty(serverVersion))
-                    {
-                        string[] serverVersions = serverVersion.Replace("v","").Split('.');
-                        string[] versions=version.Replace("v","").Split('.');
-                        for(int i=0;i<serverVersions.Length;i++)
-                        {
-                            if(versions.Length>=i+1)
-                            {
-                                int serverNum = 0;
-                                int num=0;
-                                if (Int32.TryParse(serverVersions[i], out serverNum) && Int32.TryParse(versions[i], out num))
-                                {
-                                    if(i==0)
-                                    {
-                                        if(serverNum>num)
-                                        {
-                                            isMustUpdate = true;
-                                            break;
-                                        }
-                                    }
-                                    else
-                                    {
-                                        if (serverNum > num)
-                                        {
-                                            isNeedUpdate = true;
-                                            break;
-                                        }
-                                    }
-
-                                }
-                            }
-                            else 
-                            {
-                                int serverNum = 0;
-                                int num = 0;
-                                if (Int32.TryParse(serverVersions[versions.Length - 1], out serverNum) && Int32.TryParse(versions[versions.Length - 1], out num))
-                                {
-
-                                        if (serverNum == num)
-                                        {
-                                            isNeedUpdate = true;
-                                            break;
-                                        }
-                                        else
-                                        {
-                                            break;
-                                        }
-
-                                }
-                            }
-                        }
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                }
-            }
-
-            if(isMustUpdate)
-            {
-                if (MessageBox.Show("存在必要更新，点击确定更新!","提示",MessageBoxButton.OK) == MessageBoxResult.OK)
-                {
-                    Process openupdatedexe = new Process();
-                    openupdatedexe.StartInfo.FileName = "AutoUpdate.exe";
-                    openupdatedexe.StartInfo.Arguments = "GenerateProgrammeCode.exe " + serverJObject["totalByes"].ToString();
-                    openupdatedexe.Start();
-
-                    this.Close();
-                }
-            }
-            else if(isNeedUpdate)
-            {
-                if (MessageBox.Show("存在更新，点击确定更新,点击取消不更新!\r\n更新日志：" + "\r\n" + serverJObject["log"].ToString(), "提示", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
-                {
-                    Process openupdatedexe = new Process();
-                    openupdatedexe.StartInfo.FileName = "AutoUpdate.exe";
-                    openupdatedexe.StartInfo.Arguments = "GenerateProgrammeCode.exe " + serverJObject["totalByes"].ToString();
-                    openupdatedexe.Start();
-
-                    this.Close();
-                }
-            }
-            
-
             this.Loaded += MainWindow_Loaded;
         }
 
@@ -158,11 +46,19 @@ namespace GenerateProgrammeCode
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             this.Loaded -= MainWindow_Loaded;
-            cmbViewType.ItemsSource = new List<ViewType> { new ViewType(){ Desc="生成Linq类实体",Value= "GenerateEntity"},
-                new ViewType(){ Desc="生成TableOp",Value= "GenerateTabOp"} };
+
+            CheckUpdate();
+
+            this.Title = "生成代码" + version;
+
+
+            cmbViewType.ItemsSource = new List<ViewType> { 
+                new ViewType(){ Desc="生成Linq类实体",Value= "GenerateEntity"},
+                new ViewType(){ Desc="生成TableOp",Value= "GenerateTabOp"} ,
+                new ViewType(){ Desc="生成域配置执行SQL",Value= "GenerateSXGISMIS"}};
+
             cmbViewType.SelectedValuePath = "Value";
             cmbViewType.DisplayMemberPath = "Desc";
-
         }
 
         private void cmbViewType_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -188,6 +84,130 @@ namespace GenerateProgrammeCode
 
         #endregion
 
+        #region===类公共方法===
+        //检查是否有更新
+        bool isNeedUpdate = false;
+        bool isMustUpdate = false;
+        void CheckUpdate()
+        {
+            url = ConfigurationManager.AppSettings["Url"].Trim();
+            version = ConfigurationManager.AppSettings["Version"].Trim();
+            if (string.IsNullOrEmpty(version))
+            {
+                isNeedUpdate = true;
+            }
+            else if (!string.IsNullOrEmpty(url))
+            {
+                WebClient webClient = new WebClient();
+
+                Uri address = new Uri(url);
+
+                //下载软件版本号
+                webClient.DownloadStringCompleted += webClient_DownloadStringCompleted;
+                MyBusyIndicator.IsBusy = true;
+                webClient.DownloadStringAsync(address);
+            }
+        }
+
+        //从服务器中下载版本号完成后执行
+        void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                try
+                {
+                    string serverJson = e.Result;
+
+                    serverJObject = JToken.Parse(serverJson) as JObject;
+
+                    string serverVersion = serverJObject["version"].ToString();
+                    if (!string.IsNullOrEmpty(serverVersion))
+                    {
+                        string[] serverVersions = serverVersion.Replace("v", "").Split('.');
+                        string[] versions = version.Replace("v", "").Split('.');
+                        for (int i = 0; i < serverVersions.Length; i++)
+                        {
+                            if (versions.Length >= i + 1)
+                            {
+                                int serverNum = 0;
+                                int num = 0;
+                                if (Int32.TryParse(serverVersions[i], out serverNum) && Int32.TryParse(versions[i], out num))
+                                {
+                                    if (i == 0)
+                                    {
+                                        if (serverNum > num)
+                                        {
+                                            isMustUpdate = true;
+                                            break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (serverNum > num)
+                                        {
+                                            isNeedUpdate = true;
+                                            break;
+                                        }
+                                    }
+
+                                }
+                            }
+                            else
+                            {
+                                int serverNum = 0;
+                                int num = 0;
+                                if (Int32.TryParse(serverVersions[versions.Length - 1], out serverNum) && Int32.TryParse(versions[versions.Length - 1], out num))
+                                {
+
+                                    if (serverNum == num)
+                                    {
+                                        isNeedUpdate = true;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+
+                    if (isMustUpdate)
+                    {
+                        if (MessageBox.Show("存在必要更新，点击确定更新!", "提示", MessageBoxButton.OK) == MessageBoxResult.OK)
+                        {
+                            Process openupdatedexe = new Process();
+                            openupdatedexe.StartInfo.FileName = "AutoUpdate.exe";
+                            openupdatedexe.StartInfo.Arguments = "GenerateProgrammeCode.exe " + serverJObject["totalByes"].ToString();
+                            openupdatedexe.Start();
+
+                            this.Close();
+                        }
+                    }
+                    else if (isNeedUpdate)
+                    {
+                        if (MessageBox.Show("存在更新，点击确定更新,点击取消不更新!\r\n更新日志：" + "\r\n" + serverJObject["log"].ToString(), "提示", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                        {
+                            Process openupdatedexe = new Process();
+                            openupdatedexe.StartInfo.FileName = "AutoUpdate.exe";
+                            openupdatedexe.StartInfo.Arguments = "GenerateProgrammeCode.exe " + serverJObject["totalByes"].ToString();
+                            openupdatedexe.Start();
+
+                            this.Close();
+                        }
+                    }
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            MyBusyIndicator.IsBusy = false;
+        }
+        #endregion
 
         class ViewType
         {
@@ -201,7 +221,5 @@ namespace GenerateProgrammeCode
             /// </summary>
             public string Value { get; set; }
         }
-
-
     }
 }
---
+++ Oracle/GenerateSXGISMIS.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GenerateProgrammeCode.Oracle
+{
+    /// <summary>
+    /// GenerateSXGISMIS.xaml 的交互逻辑
+    /// </summary>
+    public partial class GenerateSXGISMIS : UserControl
+    {
+        public GenerateSXGISMIS()
+        {
+            InitializeComponent();
+
+            cmbTable.ItemsSource = new List<string>() { "A10_DM", "A10C_DMVALUE", "A10CC_AUTH", "A10CC_DMVALUE", "A10CC_GROUP", "A10CC_NOTNULL" };
+        }
+
+        private void btnGenerateSXGISMIS_Click(object sender, RoutedEventArgs e)
+        {
+            rtbCode.Document.Blocks.Clear();
+            if (!string.IsNullOrEmpty(tbCode.Text)&&cmbTable.SelectedItem != null)
+            {
+                string strSelectedItem = cmbTable.SelectedItem as string;
+                switch (strSelectedItem)
+                {
+                    case "A10_DM":
+                        rtbCode.AppendText("select * from A10_DM a10 where a10.a10_code='" + tbCode.Text + "'");
+                        break;
+                    case "A10C_DMVALUE":
+                        rtbCode.AppendText("select * from A10C_DMVALUE t where t.a10_id in (select a10.a10_id from A10_DM a10 where a10.a10_code ='" + tbCode.Text + "')");
+                        break;
+                    case "A10CC_AUTH":
+                        rtbCode.AppendText("select * from A10CC_AUTH t where t.a10c_id in (select a10c.a10c_id from A10C_DMVALUE a10c where a10c.a10_id in (select a10.a10_id from A10_DM a10 where a10.a10_code ='" + tbCode.Text + "'))");
+                        break;
+                    case "A10CC_DMVALUE":
+                        rtbCode.AppendText("select * from A10CC_DMVALUE t where t.a10c_id in (select a10c.a10c_id from A10C_DMVALUE a10c where a10c.a10_id in (select a10.a10_id from A10_DM a10 where a10.a10_code ='" + tbCode.Text + "'))");
+                        break;
+                    case "A10CC_GROUP":
+                        rtbCode.AppendText("select * from A10CC_GROUP t where t.a10c_id in (select a10c.a10c_id from A10C_DMVALUE a10c where a10c.a10_id in (select a10.a10_id from A10_DM a10 where a10.a10_code ='" + tbCode.Text + "'))");
+                        break;
+                    case "A10CC_NOTNULL":
+                        rtbCode.AppendText("select * from A10CC_NOTNULL t where t.a10c_id in (select a10c.a10c_id from A10C_DMVALUE a10c where a10c.a10_id in (select a10.a10_id from A10_DM a10 where a10.a10_code ='" + tbCode.Text + "'))");
+                        break;
+                }
+            }
+        }
+    }
+}
